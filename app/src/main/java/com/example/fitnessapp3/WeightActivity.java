b26b1a85ed029b91ed042ea0e8bcdb962d400e53
Tobias <t.forner@tum.de>
2023-06-19 20:00:17 +0200

    average weights per week

--- app/src/main/java/com/example/fitnessapp3/WeightActivity.java
+++ app/src/main/java/com/example/fitnessapp3/WeightActivity.java
@@ -14,6 +14,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.androidplot.xy.BoundaryMode;
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYGraphWidget;
@@ -27,15 +28,18 @@ import org.json.JSONObject;
 import java.text.FieldPosition;
 import java.text.Format;
 import java.text.ParsePosition;
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 public class WeightActivity extends AppCompatActivity {
+
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd.HH-mm", Locale.getDefault());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +55,65 @@ public class WeightActivity extends AppCompatActivity {
 
         List<JSONObject> weights = getSortedWeightDates();
         Collections.reverse(weights);
-        List<Float> weightNumbers = weights.stream().map(x -> {
+
+        //aggregate weight averages
+        List<Float> aggregatedWeights = new ArrayList<>();
+        List<String> consideredDates = new ArrayList<>();
+        try {
+            String firstDateStr = weights.get(0).getString("date");
+            LocalDateTime firstDate = LocalDateTime.parse(firstDateStr, dateTimeFormatter);
+            LocalDateTime nextSunday = firstDate.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+            nextSunday = nextSunday.plusHours(23 - nextSunday.getHour());
+            nextSunday = nextSunday.plusMinutes(59 - nextSunday.getMinute());
+            nextSunday = nextSunday.plusSeconds(59 - nextSunday.getSecond());
+
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.getDefault());
+
+            float currentWeightSum = Float.parseFloat(weights.get(0).getString("weight"));
+            int count = 1;
+            for (int i = 1; i < weights.size(); i++) {
+                JSONObject currentEntry = weights.get(i);
+                LocalDateTime currentDate = LocalDateTime.parse(currentEntry.getString("date"), dateTimeFormatter);
+                Log.d("WeightActivity", "considering dateTime " + currentEntry.getString("date") + "; current nextSunday is " + nextSunday.format(dateTimeFormatter));
+                if (nextSunday.isAfter(currentDate)) {
+                    currentWeightSum += Float.parseFloat(currentEntry.getString("weight"));
+                    count += 1;
+                    Log.d("WeightActivity", "next Sunday " + nextSunday.format(dateTimeFormatter) + " is after current");
+                } else {
+                    if (currentDate.getDayOfWeek() == DayOfWeek.SUNDAY) {
+                        // the current date is after next sunday and on a Sunday
+                        // This means that it must be the only entry for this week
+                        consideredDates.add(nextSunday.format(dtf));
+                        aggregatedWeights.add(currentWeightSum / count);
+
+                        nextSunday = currentDate.plusHours(23 - currentDate.getHour());
+                        nextSunday = nextSunday.plusMinutes(59 - nextSunday.getMinute());
+                        nextSunday = nextSunday.plusSeconds(59 - nextSunday.getSecond());
+
+                        currentWeightSum = Float.parseFloat(currentEntry.getString("weight"));
+                        count = 1;
+                    } else {
+                        consideredDates.add(nextSunday.format(dtf));
+                        aggregatedWeights.add(currentWeightSum / count);
+
+                        currentWeightSum = Float.parseFloat(currentEntry.getString("weight"));
+                        count = 1;
+
+                        nextSunday = currentDate.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
+                        nextSunday = nextSunday.plusHours(23 - nextSunday.getHour());
+                        nextSunday = nextSunday.plusMinutes(59 - nextSunday.getMinute());
+                        nextSunday = nextSunday.plusSeconds(59 - nextSunday.getSecond());
+                    }
+                    Log.d("WeightActivity", "next Sunday is not after current, starting new week ending at " + nextSunday.format(dateTimeFormatter));
+                }
+            }
+            consideredDates.add(nextSunday.format(dtf));
+            aggregatedWeights.add(currentWeightSum / count);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        /*List<Float> weightNumbers = weights.stream().map(x -> {
             try {
                 return Float.parseFloat(x.getString("weight"));
             } catch (JSONException e) {
@@ -65,36 +127,40 @@ public class WeightActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
-        }).collect(Collectors.toList());
+        }).collect(Collectors.toList());*/
 
 
         // turn the above arrays into XYSeries':
         // (Y_VALS_ONLY means use the element index as the x value)
         XYSeries weightSeries = new SimpleXYSeries(
-                weightNumbers, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Weight");
+                aggregatedWeights, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Weight");
 
         // create formatters to use for drawing a series using LineAndPointRenderer
         // and configure them from xml:
         LineAndPointFormatter series1Format =
                 new LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels);
+        series1Format.setPointLabeler((xySeries, i) -> String.format(Locale.getDefault(), "%.01f", xySeries.getY(i).floatValue()));
 
 
         plot.addSeries(weightSeries, series1Format);
         plot.getGraph().setMarginBottom(200);
         plot.getGraph().setMarginTop(50);
+        plot.getGraph().setPaddingTop(50);
         plot.setDomainStep(INCREMENT_BY_VAL, 1);
+        plot.setRangeBoundaries(aggregatedWeights.stream().min(Float::compareTo).orElse(70.0f) - 0.5, aggregatedWeights.stream().max(Float::compareTo).orElse(82.f) + 0.5, BoundaryMode.FIXED);
+        //Log.d("WeightActivity", "" + plot.getGraph().range);
 
         plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).setFormat(new Format() {
             @Override
             public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
                 float posF = ((Number) obj).floatValue();
                 int i = Math.round(posF);
-                return toAppendTo.append(dateLabels.get(i));
+                return toAppendTo.append(consideredDates.get(i));
             }
 
             @Override
             public Object parseObject(String source, ParsePosition pos) {
-                return dateLabels.indexOf(source);
+                return consideredDates.indexOf(source);
             }
         });
         plot.redraw();
@@ -110,7 +176,6 @@ public class WeightActivity extends AppCompatActivity {
         }
 
         LocalDateTime t = LocalDateTime.now();
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd.HH-mm", Locale.getDefault());
         String date = t.format(dateTimeFormatter);
 
         JSONObject weightLog = getPastWeights(this);
@@ -131,7 +196,7 @@ public class WeightActivity extends AppCompatActivity {
         updatePlot();
         View focusView = this.getCurrentFocus();
         if (focusView != null) {
-            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             imm.hideSoftInputFromWindow(focusView.getWindowToken(), 0);
         }
     }
@@ -193,7 +258,7 @@ public class WeightActivity extends AppCompatActivity {
                 newText.append("\n");
             }
             try {
-                newText.append(entry.getString("date")).append(":  ").append(entry.getString("weight"));
+                newText.append(entry.getString("date")).append(":  ").append(String.format("%-5s", Util.strip(entry.getString("weight"))));
             } catch (JSONException e) {
                 throw new RuntimeException(e);
             }
