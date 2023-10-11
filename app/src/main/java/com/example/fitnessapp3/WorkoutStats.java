cae0c10c73d4704bd3ddb9ad05439216dee1d0e5
Tobias <t.forner@tum.de>
2023-10-11 22:55:05 +0200

    improve workout component ordering

--- app/src/main/java/com/example/fitnessapp3/WorkoutStats.java
+++ app/src/main/java/com/example/fitnessapp3/WorkoutStats.java
@@ -16,10 +16,11 @@ public class WorkoutStats {
     }
 
     public int compareTo(WorkoutStats other) {
+        if (this.count > other.count) {
+            return -1;
+        }
         if (this.lastCompletedDate.compareTo(other.lastCompletedDate) == 0) {
-            if (this.count > other.count) {
-                return -1;
-            }
+
             return Integer.compare(this.posInSortedNames, other.posInSortedNames);
         } else {
             return -1 * this.lastCompletedDate.compareTo(other.lastCompletedDate);
