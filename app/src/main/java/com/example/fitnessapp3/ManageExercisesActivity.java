623585595b32d3f6b98d447f322419c98cc56343
Tobias Forner <t.forner@tum.de>
2025-11-30 12:18:40 +0100

    chore(ManageExerciseActivity): remove unused import

--- app/src/main/java/com/example/fitnessapp3/ManageExercisesActivity.java
+++ app/src/main/java/com/example/fitnessapp3/ManageExercisesActivity.java
@@ -2,7 +2,6 @@ package com.example.fitnessapp3;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowMetrics;
